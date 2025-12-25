f94bd8b17edc0572c29789d5639e0532fdc4ef02
chamil <chamil.20@cse.mrt.ac.lk>
2025-12-25 11:14:32 +0530

    set up the pipeline run

--- next.config.js
+++ next.config.js
@@ -1,28 +1,30 @@
 /** @type {import('next').NextConfig} */
+const isFirebaseQA = process.env.NEXT_PUBLIC_ENV === "qa";
 const nextConfig = {
+  ...(isFirebaseQA ? { output: "export" } : {}),
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-        port: '',
-        pathname: '/**',
-      }
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
